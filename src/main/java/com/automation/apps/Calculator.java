8a9e9c3bacf022b7e1c1615dc922918b9f2079c3
ravikrishnabattala <ravikrishnbattala5@gmail.com>
2026-05-31 20:01:12 +0530

    calculator automation

--- src/main/java/com/automation/apps/Calculator.java
+++ src/main/java/com/automation/apps/Calculator.java
@@ -1,8 +1,21 @@
 package com.automation.apps;
 
 import com.automation.driverFactory.MobileDriverManager;
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.ElementOption;
+import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Calculator {
 
@@ -13,62 +26,25 @@ public class Calculator {
         this.driver = MobileDriverManager.getDriver();
     }
 
-    private By digit0 = By.id("digit_0");
-    private By digit1 = By.id("digit_1");
-    private By digit2 = By.id("digit_2");
-    private By digit3 = By.id("digit_3");
-    private By digit5 = By.id("digit_5");
-
-    private By plus = By.id("op_add");
-    private By multiply = By.id("op_mul");
-    private By minus = By.id("op_sub");
-    private By equals = By.id("eq");
-
-    private By result = By.id("result_final");
-
-    private By clear = By.id("clr");
-
-    public void click(By locator) {
-        driver.findElement(locator).click();
-    }
-
-    public String getResult() {
-        return driver.findElement(result).getText();
-    }
-
-    public void clearCalculation() {
-        driver.findElement(clear).click();
+    public Calculator clickDigit(Object digit) {
+        driver.findElement(AppiumBy.id("com.miui.calculator:id/btn_" + digit + "_s")).click();
+        return this;
     }
 
     public void calculateExpression() {
 
-        // 25 + 15
+        driver.findElement(AppiumBy.id("android:id/button1")).click();
+        driver.findElement(AppiumBy.id("android:id/button1")).click();
+        driver.findElement(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout")).click();
 
-        click(digit2);
-        click(digit5);
-
-        click(plus);
-
-        click(digit1);
-        click(digit5);
-
-        click(equals);
-
-        // * 3
-
-        click(multiply);
-
-        click(digit3);
-
-        click(equals);
-
-        // -10
-
-        click(minus);
-
-        click(digit1);
-        click(digit0);
-
-        click(equals);
+        // (25 + 15) × 3 − 10)
+        clickDigit(2).clickDigit(5)
+                .clickDigit("plus")
+                .clickDigit(1).clickDigit(5).clickDigit("equal")
+                .clickDigit("mul")
+                .clickDigit(3).clickDigit("equal")
+                .clickDigit("minus")
+                .clickDigit(1).clickDigit(0)
+                .clickDigit("equal");
     }
 }
--- src/main/java/com/automation/driverFactory/MobileDriverManager.java
+++ src/main/java/com/automation/driverFactory/MobileDriverManager.java
@@ -5,6 +5,7 @@ import io.appium.java_client.android.options.UiAutomator2Options;
 import lombok.Getter;
 
 import java.net.URL;
+import java.time.Duration;
 
 public class MobileDriverManager {
 
@@ -16,19 +17,19 @@ public class MobileDriverManager {
 
     public static void initAndroid() {
 
-
         UiAutomator2Options options = new UiAutomator2Options();
         options.setPlatformName("Android");
-        options.setDeviceName("Pixel_8");
+        options.setDeviceName("emulator-5554");
         options.setAutomationName("UiAutomator2");
+        options.setAppPackage("com.miui.calculator");
+        options.setAppActivity("com.miui.calculator.cal.CalculatorActivity");
 
-        options.setAppPackage("com.google.android.calculator");
-        options.setAppActivity("com.android.calculator2.Calculator");
-
+        options.setNoReset(false);
         try {
             driver = new AndroidDriver(
                     new URL("http://127.0.0.1:4723"),
                     options);
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         } catch (Exception e) {
             e.printStackTrace();
         }
